59e3033c33cc91c8e0f9d7454bd771f46fb6a5f4
Volodymyr Burmitskyi <vburmtsk@gmail.com>
2025-10-24 21:42:10 -0700

    Add caching Fibonacci function to Task 1

---
+++ task_1/task_1.py
@@ -0,0 +1,20 @@
+def caching_fibonacci():
+    cache = {}
+
+    def fibonacci(n):
+        if n <= 0:
+            return 0
+        elif n == 1:
+            return 1
+        elif n in cache:
+            return cache[n]
+
+        cache[n] = fibonacci(n - 1) + fibonacci(n - 2)
+        return cache[n]
+
+    return fibonacci
+
+
+fib = caching_fibonacci()
+print(f"{fib(10)=}")  # -> 55
+print(f"{fib(15)=}")  # -> 610
